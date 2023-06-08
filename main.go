e9bd85b288f8ce4f793befd132796fe3977108e2
asadbek21coder <asadbekegambergenov21coder@gmail.com>
2023-06-08 11:40:41 +0500

    Fibonacci

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println(CalcSum(1800, 1.5), "so`m")
+	fmt.Println(Fibo(5))
 }
 
 func CalcSum(price float64, kg float64) float64 {
 	return price * kg
+}
+
+func Fibo(num int) int {
+	return num
 }
